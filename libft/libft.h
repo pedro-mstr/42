2a08b9fa669e52aa073f54cc9e22e306a876762b
Pedro <ptos666@gmail.com>
2022-10-28 14:06:24 +0100

    28/10/2022

--- libft/libft.h
+++ libft/libft.h
@@ -12,10 +12,19 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
-#include	<string.h>
+# include <string.h>
 
-size_t	ft_strlen(const char *str);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-size_t	t_strlcpy(char *dst, const char *src, size_t dstsize);
-long	ft_atoi(const char *str);
+size_t		ft_strlen(const char *str);
+size_t		ft_strlcat(char *dest, const char *src, size_t size);
+size_t		t_strlcpy(char *dst, const char *src, size_t dstsize);
+char		*ft_strnstr(const char *str, const char *to_find, size_t len);
+long		ft_atoi(const char *str);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+void		ft_bzero(void *s, size_t n);
 #endif
